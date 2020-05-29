cca08a1d71114939f1a86e05b228c1f80aedadb1
Arsalan <arsalankhanji@gmail.com>
2020-05-30 01:32:16 +0200

    Added PID control

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from Servo_Control import servoMotor
 from time import sleep, time
 
 sleep_time = 0 #0.0001
+dt = 0.001
 
 [bx, by, bz] = Cimu.init_imu() 
 
@@ -10,13 +11,35 @@ servoA = servoMotor.ServoMotor(18)
 servoB = servoMotor.ServoMotor(17)
 mean = 1500  # corresponding to 90 degrees
 
+phiSetpoint = 0 # degrees
+thetaSetpoint = 0 # degrees
+
+# Control Gains
+Kp = -20
+Ki = 0.1
+Kd = 0.1
+
 try:
+    previous_errorPhi = 0
+    integralPhi = 0
     while True:
+        # Getting Attitude of Floater
         [Phi , Theta ] = Cimu.get_imu(bx, by, bz) # Phi about x  &   Theta about y
         print("Phi: " + str(round(Phi,1)) + " | Theta: " + str(round(Theta,1)) )
+
+        # Error Signal for Controls
+        errorPhi = phiSetpoint - Phi # error = setpoint - measured_value
+        integralPhi = integralPhi + errorPhi*dt
+        derivativePhi = (errorPhi - previous_errorPhi)/dt
+        outputPhi = Kp*errorPhi + Ki*integralPhi + Kd*derivativePhi
+        previous_errorPhi = errorPhi
+
+	# Servo Actuation
+        servoA.moveServo(mean + outputPhi) #Phi*10)
+        servoB.moveServo(mean + 0*Theta*10 + 100)
+
         sleep(sleep_time)
-        servoA.moveServo(mean + Phi*10)
-        servoB.moveServo(mean + Theta*10)
+        sleep(dt)
 
 except:
     servoA.stopServo()
